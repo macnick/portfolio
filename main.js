6ac701c5457d9ba6ccd7654d279a91ca7818b674
macnick <nick@macexperts.gr>
2020-11-03 11:17:27 +0200

    Add social icons

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 const head = document.querySelector('.header');
 
 head.addEventListener('click', () => console.log('hello'));
+
+// $(function () {
+//   // this will get the full URL at the address bar
+//   var url = window.location.href;
+
+//   // passes on every "a" tag
+//   $('.nav-items a').each(function () {
+//     // checks if its the same on the address bar
+//     if (url == this.href) {
+//       $(this).closest('li').addClass('active');
+//     }
+//   });
+// });
